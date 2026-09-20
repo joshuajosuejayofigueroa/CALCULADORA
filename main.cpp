2afd4ce5c2fb31464bf2b1f81debed120399193d
joshuajosuejayofigueroa <joshuajosuejayofigueroa@gmail.com>
2026-09-19 20:47:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
-#include <iostream>
 #include "calculadora.h"
 
-using namespace std;
-
 int main() {
-    double respuestaAnterior = 0.0;
-    double memoria1 = 0.0;
-    double memoria2 = 0.0;
-    
     int opcionPrincipal = 0;
 
-    while (opcionPrincipal != 4) {
-        cout << "\n=== CALCULADORA CIENTIFICA Y MATRICIAL ===\n";
-        cout << "1. Modulo Cientifico\n";
-        cout << "2. Modulo de Matrices\n";
-        cout << "3. Ver Variables de Memoria\n";
-        cout << "4. Salir\n";
+    double Ans = 0.0;
+    double M1 = 0.0;
+    double M2 = 0.0;
+
+    do {
+        cout << "\n--- CALCULADORA CIENTIFICA Y MATRICIAL ---\n";
+        cout << "1. Operaciones Basicas\n";
+        cout << "2. Modulo Cientifico\n";
+        cout << "3. Modulo de Matrices\n";
+        cout << "4. Gestion de Memoria\n";
+        cout << "5. Salir\n";
         cout << "Ingrese una opcion: ";
         cin >> opcionPrincipal;
 
         if (opcionPrincipal == 1) {
-            int opcionCientifica = 0;
+            operacionesBasicas(Ans);
+
+        } else if (opcionPrincipal == 2) {
+            int opCientifica = 0;
             cout << "\n--- MODULO CIENTIFICO ---\n";
             cout << "1. Potencia\n";
             cout << "2. Factorial\n";
@@ -29,65 +30,44 @@ int main() {
             cout << "5. Exponencial (e^x)\n";
             cout << "6. Logaritmo Natural (ln)\n";
             cout << "Ingrese opcion: ";
-            cin >> opcionCientifica;
+            cin >> opCientifica;
 
-            if (opcionCientifica == 1) {
+            if (opCientifica == 1) {
                 double base;
                 int exponente;
-                cout << "Ingrese la base: ";
-                cin >> base;
-                cout << "Ingrese el exponente entero: ";
-                cin >> exponente;
-                respuestaAnterior = potencia(base, exponente);
-                cout << "Resultado: " << respuestaAnterior << "\n";
-            } else if (opcionCientifica == 2) {
+                cout << "Ingrese la base: "; cin >> base;
+                cout << "Ingrese el exponente: "; cin >> exponente;
+                Ans = potencia(base, exponente);
+                cout << "Resultado: " << Ans << endl;
+            } else if (opCientifica == 2) {
                 int numero;
-                cout << "Ingrese un numero entero positivo: ";
-                cin >> numero;
-                if (numero < 0) {
-                    cout << "Error: Numero negativo.\n";
-                } else {
-                    respuestaAnterior = factorial(numero);
-                    cout << "Resultado: " << respuestaAnterior << "\n";
-                }
-            } else if (opcionCientifica == 3) {
-                double x;
-                cout << "Ingrese x (en radianes): ";
-                cin >> x;
-                respuestaAnterior = seno(x);
-                cout << "Resultado: " << respuestaAnterior << "\n";
-            } else if (opcionCientifica == 4) {
-                double x;
-                cout << "Ingrese x (en radianes): ";
-                cin >> x;
-                respuestaAnterior = coseno(x);
-                cout << "Resultado: " << respuestaAnterior << "\n";
-            } else if (opcionCientifica == 5) {
-                double x;
-                cout << "Ingrese x: ";
-                cin >> x;
-                respuestaAnterior = exponencial(x);
-                cout << "Resultado: " << respuestaAnterior << "\n";
-            } else if (opcionCientifica == 6) {
-                double x;
-                cout << "Ingrese x: ";
-                cin >> x;
-                if (x <= 0) {
-                    cout << "Error: El numero debe ser mayor a 0.\n";
-                } else {
-                    respuestaAnterior = logaritmo(x);
-                    cout << "Resultado: " << respuestaAnterior << "\n";
-                }
+                cout << "Ingrese un numero entero: "; cin >> numero;
+                Ans = factorial(numero);
+                cout << "Resultado: " << Ans << endl;
+            } else if (opCientifica == 3) {
+                double angulo;
+                cout << "Ingrese el angulo en radianes: "; cin >> angulo;
+                Ans = seno(angulo);
+                cout << "Resultado: " << Ans << endl;
+            } else if (opCientifica == 4) {
+                double angulo;
+                cout << "Ingrese el angulo en radianes: "; cin >> angulo;
+                Ans = coseno(angulo);
+                cout << "Resultado: " << Ans << endl;
+            } else if (opCientifica == 5) {
+                double valor;
+                cout << "Ingrese el valor de x: "; cin >> valor;
+                Ans = exponencial(valor);
+                cout << "Resultado: " << Ans << endl;
+            } else if (opCientifica == 6) {
+                double valor;
+                cout << "Ingrese el valor de x (|x| < 1): "; cin >> valor;
+                Ans = logaritmo(valor);
+                cout << "Resultado: " << Ans << endl;
             }
 
-            cout << "\nGuardar resultado en memoria? (1: Guardar en M1, 2: Guardar en M2, 0: No guardar): ";
-            int opcionMemoria;
-            cin >> opcionMemoria;
-            if (opcionMemoria == 1) memoria1 = respuestaAnterior;
-            if (opcionMemoria == 2) memoria2 = respuestaAnterior;
-
-        } else if (opcionPrincipal == 2) {
-            int opcionMatrices = 0;
+        } else if (opcionPrincipal == 3) {
+            int opMatrices = 0;
             cout << "\n--- MODULO DE MATRICES ---\n";
             cout << "1. Sumar Matrices\n";
             cout << "2. Restar Matrices\n";
@@ -96,118 +76,98 @@ int main() {
             cout << "5. Traza\n";
             cout << "6. Determinante\n";
             cout << "Ingrese opcion: ";
-            cin >> opcionMatrices;
+            cin >> opMatrices;
 
-            if (opcionMatrices == 1 || opcionMatrices == 2) {
+            if (opMatrices == 1) {
                 int filas, columnas;
-                cout << "Ingrese numero de filas: ";
-                cin >> filas;
-                cout << "Ingrese numero de columnas: ";
-                cin >> columnas;
-
-                double** matrizA = crearMatriz(filas, columnas);
-                double** matrizB = crearMatriz(filas, columnas);
-
-                cout << "\nDatos de la Matriz A:\n";
-                leerMatriz(matrizA, filas, columnas);
-                cout << "\nDatos de la Matriz B:\n";
-                leerMatriz(matrizB, filas, columnas);
-
-                double** resultado = nullptr;
-                if (opcionMatrices == 1) {
-                    resultado = sumarMatrices(matrizA, matrizB, filas, columnas);
-                    cout << "\nResultado de la Suma:\n";
+                cout << "Ingrese filas y columnas: "; cin >> filas >> columnas;
+                double** A = crearMatriz(filas, columnas);
+                double** B = crearMatriz(filas, columnas);
+                cout << "Matriz A:\n"; leerMatriz(A, filas, columnas);
+                cout << "Matriz B:\n"; leerMatriz(B, filas, columnas);
+                double** R = sumarMatrices(A, B, filas, columnas);
+                cout << "Resultado:\n"; mostrarMatriz(R, filas, columnas);
+                liberarMatriz(A, filas); liberarMatriz(B, filas); liberarMatriz(R, filas);
+            } else if (opMatrices == 2) {
+                int filas, columnas;
+                cout << "Ingrese filas y columnas: "; cin >> filas >> columnas;
+                double** A = crearMatriz(filas, columnas);
+                double** B = crearMatriz(filas, columnas);
+                cout << "Matriz A:\n"; leerMatriz(A, filas, columnas);
+                cout << "Matriz B:\n"; leerMatriz(B, filas, columnas);
+                double** R = restarMatrices(A, B, filas, columnas);
+                cout << "Resultado:\n"; mostrarMatriz(R, filas, columnas);
+                liberarMatriz(A, filas); liberarMatriz(B, filas); liberarMatriz(R, filas);
+            } else if (opMatrices == 3) {
+                int filasA, columnasA, filasB, columnasB;
+                cout << "Filas y columnas de A: "; cin >> filasA >> columnasA;
+                cout << "Filas y columnas de B: "; cin >> filasB >> columnasB;
+                if (columnasA != filasB) {
+                    cout << "No se pueden multiplicar las matrices.\n";
                 } else {
-                    resultado = restarMatrices(matrizA, matrizB, filas, columnas);
-                    cout << "\nResultado de la Resta:\n";
+                    double** A = crearMatriz(filasA, columnasA);
+                    double** B = crearMatriz(filasB, columnasB);
+                    cout << "Matriz A:\n"; leerMatriz(A, filasA, columnasA);
+                    cout << "Matriz B:\n"; leerMatriz(B, filasB, columnasB);
+                    double** R = multiplicarMatrices(A, filasA, columnasA, B, filasB, columnasB);
+                    cout << "Resultado:\n"; mostrarMatriz(R, filasA, columnasB);
+                    liberarMatriz(A, filasA); liberarMatriz(B, filasB); liberarMatriz(R, filasA);
                 }
-
-                imprimirMatriz(resultado, filas, columnas);
-
-                liberarMatriz(matrizA, filas);
-                liberarMatriz(matrizB, filas);
-                liberarMatriz(resultado, filas);
-
-            } else if (opcionMatrices == 3) {
-                int filasA, columnasA, columnasB;
-                cout << "Ingrese filas de Matriz A: ";
-                cin >> filasA;
-                cout << "Ingrese columnas de Matriz A (y filas de Matriz B): ";
-                cin >> columnasA;
-                cout << "Ingrese columnas de Matriz B: ";
-                cin >> columnasB;
-
-                double** matrizA = crearMatriz(filasA, columnasA);
-                double** matrizB = crearMatriz(columnasA, columnasB);
-
-                cout << "\nDatos de la Matriz A:\n";
-                leerMatriz(matrizA, filasA, columnasA);
-                cout << "\nDatos de la Matriz B:\n";
-                leerMatriz(matrizB, columnasA, columnasB);
-
-                double** resultado = multiplicarMatrices(matrizA, matrizB, filasA, columnasA, columnasB);
-
-                cout << "\nResultado de la Multiplicacion:\n";
-                imprimirMatriz(resultado, filasA, columnasB);
-
-                liberarMatriz(matrizA, filasA);
-                liberarMatriz(matrizB, columnasA);
-                liberarMatriz(resultado, filasA);
-
-            } else if (opcionMatrices == 4) {
+            } else if (opMatrices == 4) {
                 int filas, columnas;
-                cout << "Ingrese filas: ";
-                cin >> filas;
-                cout << "Ingrese columnas: ";
-                cin >> columnas;
-
-                double** matriz = crearMatriz(filas, columnas);
-                cout << "\nDatos de la Matriz:\n";
-                leerMatriz(matriz, filas, columnas);
-
-                double** resultado = obtenerTranspuesta(matriz, filas, columnas);
-                cout << "\nMatriz Transpuesta:\n";
-                imprimirMatriz(resultado, columnas, filas);
-
-                liberarMatriz(matriz, filas);
-                liberarMatriz(resultado, columnas);
-
-            } else if (opcionMatrices == 5) {
-                int dimension;
-                cout << "Ingrese la dimension de la matriz cuadrada: ";
-                cin >> dimension;
-
-                double** matriz = crearMatriz(dimension, dimension);
-                cout << "\nDatos de la Matriz:\n";
-                leerMatriz(matriz, dimension, dimension);
-
-                double traza = obtenerTraza(matriz, dimension);
-                cout << "\nLa Traza es: " << traza << "\n";
-
-                liberarMatriz(matriz, dimension);
-
-            } else if (opcionMatrices == 6) {
-                int dimension;
-                cout << "Ingrese la dimension de la matriz cuadrada: ";
-                cin >> dimension;
-
-                double** matriz = crearMatriz(dimension, dimension);
-                cout << "\nDatos de la Matriz:\n";
-                leerMatriz(matriz, dimension, dimension);
-
-                double determinante = obtenerDeterminante(matriz, dimension);
-                cout << "\nEl Determinante es: " << determinante << "\n";
-
-                liberarMatriz(matriz, dimension);
+                cout << "Ingrese filas y columnas: "; cin >> filas >> columnas;
+                double** A = crearMatriz(filas, columnas);
+                cout << "Matriz A:\n"; leerMatriz(A, filas, columnas);
+                double** T = transponerMatriz(A, filas, columnas);
+                cout << "Transpuesta:\n"; mostrarMatriz(T, columnas, filas);
+                liberarMatriz(A, filas); liberarMatriz(T, columnas);
+            } else if (opMatrices == 5) {
+                int tamanio;
+                cout << "Ingrese tamanio de matriz cuadrada: "; cin >> tamanio;
+                double** A = crearMatriz(tamanio, tamanio);
+                cout << "Matriz A:\n"; leerMatriz(A, tamanio, tamanio);
+                Ans = calcularTraza(A, tamanio);
+                cout << "Traza: " << Ans << endl;
+                liberarMatriz(A, tamanio);
+            } else if (opMatrices == 6) {
+                int tamanio;
+                cout << "Ingrese tamanio de matriz cuadrada: "; cin >> tamanio;
+                double** A = crearMatriz(tamanio, tamanio);
+                cout << "Matriz A:\n"; leerMatriz(A, tamanio, tamanio);
+                Ans = determinante(A, tamanio);
+                cout << "Determinante: " << Ans << endl;
+                liberarMatriz(A, tamanio);
             }
 
-        } else if (opcionPrincipal == 3) {
-            cout << "\n--- MEMORIA Y VARIABLES ---\n";
-            cout << "Ans (Ultima respuesta): " << respuestaAnterior << "\n";
-            cout << "M1: " << memoria1 << "\n";
-            cout << "M2: " << memoria2 << "\n";
-        }
-    }
+        } else if (opcionPrincipal == 4) {
+            int opMemoria = 0;
+            cout << "\n--- GESTION DE MEMORIA ---\n";
+            cout << "1. Ver valores\n";
+            cout << "2. Guardar Ans en M1\n";
+            cout << "3. Guardar Ans en M2\n";
+            cout << "4. Limpiar memorias\n";
+            cout << "Ingrese opcion: ";
+            cin >> opMemoria;
 
+            if (opMemoria == 1) {
+                cout << "Ans = " << Ans << endl;
+                cout << "M1  = " << M1 << endl;
+                cout << "M2  = " << M2 << endl;
+            } else if (opMemoria == 2) {
+                M1 = Ans;
+                cout << "Ans guardado en M1.\n";
+            } else if (opMemoria == 3) {
+                M2 = Ans;
+                cout << "Ans guardado en M2.\n";
+            } else if (opMemoria == 4) {
+                M1 = 0.0;
+                M2 = 0.0;
+                cout << "Memorias reiniciadas.\n";
+            }
+        }
+
+    } while (opcionPrincipal != 5);
+
+    cout << "Gracias por usar la calculadora.\n";
     return 0;
 }
