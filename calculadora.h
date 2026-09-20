de5d0d77ad3d30a7b030f23ba3b967bf060cf159
joshuajosuejayofigueroa <joshuajosuejayofigueroa@gmail.com>
2026-09-19 20:45:41 -0500

    calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -2,26 +2,26 @@
 #define CALCULADORA_H
 
 #include <iostream>
-
 using namespace std;
 
 double potencia(double base, int exponente);
-double factorial(int numero);
-double seno(double x);
-double coseno(double x);
-double exponencial(double x);
-double logaritmo(double x);
+int factorial(int n);
+double seno(double x, int n = 10);
+double coseno(double x, int n = 10);
+double exponencial(double x, int n = 15);
+double logaritmo(double x, int n = 20);
 
 double** crearMatriz(int filas, int columnas);
 void liberarMatriz(double** matriz, int filas);
 void leerMatriz(double** matriz, int filas, int columnas);
-void imprimirMatriz(double** matriz, int filas, int columnas);
+void mostrarMatriz(double** matriz, int filas, int columnas);
+void operacionesBasicas(double& Ans);
 
-double** sumarMatrices(double** matrizA, double** matrizB, int filas, int columnas);
-double** restarMatrices(double** matrizA, double** matrizB, int filas, int columnas);
-double** multiplicarMatrices(double** matrizA, double** matrizB, int filasA, int columnasA, int columnasB);
-double** obtenerTranspuesta(double** matriz, int filas, int columnas);
-double obtenerTraza(double** matriz, int filas);
-double obtenerDeterminante(double** matriz, int dimension);
+double** sumarMatrices(double** A, double** B, int filas, int columnas);
+double** restarMatrices(double** A, double** B, int filas, int columnas);
+double** multiplicarMatrices(double** A, int fA, int cA, double** B, int fB, int cB);
+double** transponerMatriz(double** A, int filas, int columnas);
+double calcularTraza(double** A, int n);
+double determinante(double** A, int n);
 
 #endif
