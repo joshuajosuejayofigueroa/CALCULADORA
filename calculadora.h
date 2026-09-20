fd47ae0050ef7f190e4ec4112afb7201b313897b
joshuajosuejayofigueroa <joshuajosuejayofigueroa@gmail.com>
2026-09-20 08:46:38 -0500

    calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -2,21 +2,21 @@
 #define CALCULADORA_H
 
 #include <iostream>
-#include <string>
 using namespace std;
+
+void operacionesBasicas(double& Ans);
 
 double potencia(double base, int exponente);
 int factorial(int n);
 double seno(double x, int n = 10);
 double coseno(double x, int n = 10);
-double exponencial(double x, int n = 15);
-double logaritmo(double x, int n = 20);
+double exponencial(double x, int n = 10);
+double logaritmo(double x, int n = 10);
 
 double** crearMatriz(int filas, int columnas);
 void liberarMatriz(double** matriz, int filas);
 void leerMatriz(double** matriz, int filas, int columnas);
 void mostrarMatriz(double** matriz, int filas, int columnas);
-void operacionesBasicas(double& Ans);
 
 double** sumarMatrices(double** A, double** B, int filas, int columnas);
 double** restarMatrices(double** A, double** B, int filas, int columnas);
@@ -25,7 +25,5 @@ double** transponerMatriz(double** A, int filas, int columnas);
 double calcularTraza(double** A, int n);
 double determinante(double** A, int n);
 void resolverGaussJordan();
-
-double evaluarExpresion(const string &texto, double &Ans, double &M1, double &M2);
 
 #endif
