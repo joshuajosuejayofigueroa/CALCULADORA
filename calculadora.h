38c214801810506d6da80d84568f7b9674eb057a
joshuajosuejayofigueroa <joshuajosuejayofigueroa@gmail.com>
2026-09-20 12:10:19 -0500

    calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -4,20 +4,29 @@
 #include <iostream>
 using namespace std;
 
+// OPERACIONES BASICAS Y EVALUADOR DE EXPRESIONES
 void operacionesBasicas(double& Ans);
+double evaluarExpresion(char texto[], int& posicion);
+double evaluarTermino(char texto[], int& posicion);
+double evaluarFactor(char texto[], int& posicion);
 
+// MODULO CIENTIFICO
 double potencia(double base, int exponente);
 double factorial(int n);
-double seno(double x, int n = 10);
-double coseno(double x, int n = 10);
-double exponencial(double x, int n = 10);
-double logaritmo(double x, int n = 10);
+double senoTaylor(double x, int n);
+double seno(double grados, int n);
+double cosenoTaylor(double x, int n);
+double coseno(double grados, int n);
+double exponencialTaylor(double x, int n);
+double exponencial(double x, int grado);
+double logaritmoTaylor(double y, int n);
+double logaritmo(double x, int grado);
 
+// MODULO DE MATRICES
 double** crearMatriz(int filas, int columnas);
 void liberarMatriz(double** matriz, int filas);
 void leerMatriz(double** matriz, int filas, int columnas);
 void mostrarMatriz(double** matriz, int filas, int columnas);
-
 double** sumarMatrices(double** A, double** B, int filas, int columnas);
 double** restarMatrices(double** A, double** B, int filas, int columnas);
 double** multiplicarMatrices(double** A, int fA, int cA, double** B, int fB, int cB);
